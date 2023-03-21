c500eb24d30b0c72be1d78f292407f50f134c2a6
CareerSuccess123 <125631254+CareerSuccess123@users.noreply.github.com>
2023-03-21 22:48:53 +0530

    Create style.dart

---
+++ style.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:careersuccess/drive.dart';
+
+TextStyle kTitleStyle = TextStyle(
+  color: Color(0xff03243f),
+  fontWeight: FontWeight.bold,
+);
+
+TextStyle kFilterStyle = TextStyle(
+  color: Color(0xff03243f),
+  fontWeight: FontWeight.w500,
+);
